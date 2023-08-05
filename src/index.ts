6a0e749262727795b1f91af3d90399d396d0c605
zulfikarrosadi <zulfikarrosadi31@gmail.com>
2023-08-05 08:07:51 +0700

    added cookieParser middleware

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 import express, { Express } from 'express';
 import cors from 'cors';
 import routes from './routes';
+import cookieParser from 'cookie-parser';
 
 const app: Express = express();
 
 app.use(express.json());
+app.use(cookieParser());
 app.use(cors());
 
 app.listen(3000, function () {
