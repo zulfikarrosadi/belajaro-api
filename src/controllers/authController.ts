9ad7e6782f19638d4a51523fa10e9c22900b0c44
zulfikarrosadi <zulfikarrosadi31@gmail.com>
2023-08-05 08:14:55 +0700

    added auth controller and added refreshTokenHandler to issue new access token

---
+++ src/controllers/authController.ts
@@ -0,0 +1,29 @@
+import { users } from '@prisma/client';
+import { NextFunction, Request, Response } from 'express';
+import { defaultResponse } from '../global';
+import { createToken, verifyToken } from '../services/authService';
+
+export async function signInHandler(
+  req: Request<{}, {}, users>,
+  res: Response<defaultResponse>,
+) {}
+
+export async function refreshTokenHandler(req: Request, res: Response) {
+  const { refreshToken } = req.cookies;
+  const { payload } = verifyToken(refreshToken);
+  if (!payload) {
+    return res.status(401).send('unauthorized');
+  }
+  const { accessToken: newAccessToken } = createToken({
+    email: payload.email,
+    id: payload.id,
+  });
+
+  return res
+    .status(200)
+    .cookie('accessToken', newAccessToken, {
+      httpOnly: true,
+      sameSite: 'none',
+    })
+    .send('token refreshed');
+}
