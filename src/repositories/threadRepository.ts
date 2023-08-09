cce3c3b7cd3b4c4c1c5837de1ac2018eff678d75
zulfikarrosadi <zulfikarrosadi31@gmail.com>
2023-08-09 11:16:14 +0700

    now all repository just return everything that db is giving, error handling will be moved to service layer instead

--- src/repositories/threadRepository.ts
+++ src/repositories/threadRepository.ts
@@ -5,22 +5,18 @@ import NotFound from '../errors/notFoundError';
 const prisma = new PrismaClient();
 
 export async function getThreads() {
-  const posts = await prisma.thread.findMany();
-  return posts;
+  const threads = await prisma.thread.findMany();
+  return threads;
 }
 
 export async function getThreadById({ id }: { id: number }) {
-  try {
-    const thread = await prisma.thread.findFirst({
-      where: { id },
-    });
-    if (!thread) {
-      throw new NotFound('post not found');
-    }
-    return thread;
-  } catch (error) {
-    return error;
+  const thread = await prisma.thread.findFirst({
+    where: { id },
+  });
+  if (!thread) {
+    throw new NotFound('post not found');
   }
+  return thread;
 }
 
 export async function createThread(
@@ -48,39 +44,27 @@ export async function createThread(
 export async function updateThread(
   thread: Thread,
 ): Promise<Pick<Thread, 'id'> | BadRequest> {
-  try {
-    const postID = await prisma.thread.update({
-      where: {
-        id: thread.id,
-      },
-      data: {
-        title: thread.title,
-        content: thread.content,
-        updated_at: new Date().toISOString(),
-        published: thread.published,
-      },
-    });
-    if (!postID) {
-      throw new Error('bad request');
-    }
-    return postID;
-  } catch (error: any) {
-    const badRequestError = new BadRequest(error.message);
-    return badRequestError;
-  }
+  const threadId = await prisma.thread.update({
+    where: {
+      id: thread.id,
+    },
+    data: {
+      title: thread.title,
+      content: thread.content,
+      updated_at: new Date().toISOString(),
+      published: thread.published,
+    },
+  });
+  return threadId;
 }
 
 export async function deleteThread(threadId: number) {
-  try {
-    const deletedThread = await prisma.thread.delete({
-      where: { id: threadId },
-      select: { id: true },
-    });
-    if (!deletedThread) {
-      throw new NotFound('post not found');
-    }
-    return deletedThread.id;
-  } catch (error) {
-    return error;
+  const deletedThread = await prisma.thread.delete({
+    where: { id: threadId },
+    select: { id: true },
+  });
+  if (!deletedThread) {
+    throw new NotFound('post not found');
   }
+  return deletedThread.id;
 }
