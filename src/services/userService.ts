1d5dbe11a03639df9576faef9d8b5027a9419719
zulfikarrosadi <zulfikarrosadi31@gmail.com>
2023-08-13 22:22:31 +0700

    add updateUsrSerivce service to handling user update info request

--- src/services/userService.ts
+++ src/services/userService.ts
@@ -1,10 +1,11 @@
-import { users } from '@prisma/client';
+import { User } from '@prisma/client';
 import BadRequest from '../errors/badRequestError';
+import ErrorFactory from '../errors/errorFactory';
 import { defaultResponse } from '../global';
 import { hashPassword } from '../helpers/passwordHelper';
-import { createUser } from '../repositories/userRepository';
+import { createUser, updateUser } from '../repositories/userRepository';
 
-export async function createUserService(user: users): Promise<defaultResponse> {
+export async function createUserService(user: User): Promise<defaultResponse> {
   const hashedPassword = await hashPassword(user.password);
   user.password = hashedPassword;
 
@@ -20,6 +21,28 @@ export async function createUserService(user: users): Promise<defaultResponse> {
         message: 'email already exists',
         details: [{ path: 'email' }],
       },
+    };
+  }
+}
+
+export async function updateUserService(user: User): Promise<defaultResponse> {
+  const hashedPassword = await hashPassword(user.password);
+  user.password = hashedPassword;
+  try {
+    const updatedUser = await updateUser(user);
+    if (!updatedUser) {
+      throw ErrorFactory.createNotFoundError('fail to update user');
+    }
+    return {
+      status: 'success',
+      code: 200,
+      data: { id: updatedUser.id, email: updatedUser.email },
+    };
+  } catch (error) {
+    return {
+      status: 'fail',
+      code: 404,
+      error: { message: 'fail to update user' },
     };
   }
 }
