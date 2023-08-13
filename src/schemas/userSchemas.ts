9bea6893c488dd4b1a9a2f1df325d598fd885aeb
zulfikarrosadi <zulfikarrosadi31@gmail.com>
2023-08-13 22:20:33 +0700

    add updateUserSchema validation schema

--- src/schemas/userSchemas.ts
+++ src/schemas/userSchemas.ts
@@ -29,3 +29,25 @@ export const userSignInSchema = object({
     password: string({ required_error: 'Password is required' }),
   }),
 });
+
+export const updateUserSchema = object({
+  body: object({
+    email: string({ required_error: 'Email is required' })
+      .email('Your email format is invalid')
+      .transform((val) => val.toLowerCase()),
+    password: string({ required_error: 'Password is required' }).min(
+      6,
+      'Password should have 6 min characters',
+    ),
+    passwordConfirmation: string({
+      required_error: 'Password Confirmation is required',
+    }),
+    firstname: string({
+      required_error: 'Firstname is required',
+    }),
+    lastname: string().optional(),
+  }).refine((body) => body.password === body.passwordConfirmation, {
+    path: ['password', 'passwordConfirmation'],
+    message: 'Password and password confirmation is not match',
+  }),
+});
