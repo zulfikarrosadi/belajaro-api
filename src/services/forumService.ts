d7c8012d37cf5c4b97c99cb3f62e5af8fc2a21ff
zulfikarrosadi <zulfikarrosadi31@gmail.com>
2023-08-23 15:13:19 +0700

    add createForumService

---
+++ src/services/forumService.ts
@@ -0,0 +1,26 @@
+import ErrorFactory from '../errors/errorFactory';
+import { defaultResponse } from '../global';
+import { createForum, ForumData } from '../repositories/forumRepository';
+
+export async function createForumService(
+  data: ForumData,
+): Promise<defaultResponse> {
+  try {
+    const result = await createForum(data);
+    if (!result) {
+      throw ErrorFactory.createBadRequestError('cannot create forum', [
+        { path: 'name', value: data.forumName },
+        { path: 'description', value: data.description },
+      ]);
+    }
+    return { status: 'success', code: 201, data: result };
+  } catch (error: any) {
+    console.log(error);
+
+    return {
+      status: 'fail',
+      code: 400,
+      error: { message: error.message, details: error.details },
+    };
+  }
+}
