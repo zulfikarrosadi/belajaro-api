2ed08e52528b90174362a454671f5c59de27622c
zulfikarrosadi <zulfikarrosadi31@gmail.com>
2023-08-23 15:14:53 +0700

    added createForum repository

---
+++ src/repositories/forumRepository.ts
@@ -0,0 +1,33 @@
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+export type ForumData = {
+  forumName: string;
+  summary: string;
+  description: string;
+  profilePicture: string;
+  banner: string;
+  tags: string[];
+};
+
+export async function createForum(
+  forumData: ForumData,
+): Promise<{ id: number }> {
+  const result = await prisma.forum.create({
+    data: {
+      name: forumData.forumName,
+      summary: forumData.summary,
+      description: forumData.description,
+      profilePicture: forumData.profilePicture,
+      banner: forumData.banner,
+      forum_tags: {
+        createMany: {
+          data: forumData.tags.map((tag) => ({ tag_id: parseInt(tag) })),
+        },
+      },
+    },
+    select: { id: true },
+  });
+  return result;
+}
