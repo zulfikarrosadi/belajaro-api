f6a58e96dd78d19c916dfdce7981130889ce3e9c
zulfikarrosadi <zulfikarrosadi31@gmail.com>
2023-08-26 22:06:49 +0700

    add editForumHandler

--- src/controllers/forumController.ts
+++ src/controllers/forumController.ts
@@ -28,3 +28,24 @@ export async function createForumHandler(
   const response = await createForumService(forumdata);
   return res.status(response.code!).json(response);
 }
+
+export async function updateForumHandler(
+  req: Request<{ forumId: string }, {}, ForumData>,
+  res: Response<defaultResponse>,
+) {
+  const forumdata: ForumData = {
+    forumId: parseInt(req.params.forumId, 10),
+    forumName: req.body.forumName,
+    summary: req.body.summary,
+    description: req.body.description,
+    tags: req.body.tags,
+  };
+  if (req.files?.forumBanner) {
+    forumdata.banner = req.files?.forumBanner[0].filename;
+  }
+  if (req.files?.forumProfilePicture) {
+    forumdata.profilePicture = req.files?.forumProfilePicture[0].filename;
+  }
+  const response = await updateForumService(forumdata);
+  return res.status(response.code!).json(response);
+}
