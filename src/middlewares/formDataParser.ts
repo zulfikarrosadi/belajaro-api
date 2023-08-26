863d0839494de35cb9ef307fc23a32d5d7d438d8
zulfikarrosadi <zulfikarrosadi31@gmail.com>
2023-08-26 21:46:14 +0700

    fix error upload response data following api spec

--- src/middlewares/formDataParser.ts
+++ src/middlewares/formDataParser.ts
@@ -1,11 +1,17 @@
 import { NextFunction, Request, Response } from 'express';
+import { defaultResponse } from '../global';
 
 export function formDataParser(multer: any) {
-  return function (req: Request, res: Response, next: NextFunction) {
+  return function (
+    req: Request,
+    res: Response<defaultResponse>,
+    next: NextFunction,
+  ) {
     multer(req, res, async function (err: any) {
       if (err) {
-        console.log(err);
-        return res.status(400).json({ error: err, message: 'dari routes' });
+        return res
+          .status(400)
+          .json({ status: 'fail', error: { message: err.message } });
       }
       return next();
     });
